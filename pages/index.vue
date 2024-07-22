acdf7038a9944a69239b5458a639b6e8152c40a1
Westerly <1715288063@qq.com>
2024-07-23 03:00:44 +0800

    chore: init

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="container">
+		<view class="test">
+			
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+	.container {
+		
+	}
+</style>
